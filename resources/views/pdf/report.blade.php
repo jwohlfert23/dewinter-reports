222e6b4c799ba33349ac063fd1dfc03bf0f40d41
Jack Wohlfert <jack.wohlfert@gmail.com>
2025-12-07 19:09:50 -0500

    Fix

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weekly Report - {{ $positionTitle }}</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800" rel="stylesheet">
     <style>
         * {
             margin: 0;
