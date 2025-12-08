e6b34554121ef47bba40c9081fdfa376f988ae84
Jack Wohlfert <jack.wohlfert@gmail.com>
2025-12-07 19:16:46 -0500

    Fix

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weekly Report - {{ $positionTitle }}</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
     <style>
       * {
         margin: 0;
